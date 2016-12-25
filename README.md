23bbff7921ac7df64b07159445ddc360dfe5e0b5
ngarg <ngarg@calpoly.edu>
2016-12-25 10:58:39 -0800

    Added code framework.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Earthworm: Code Decomposition Tool
+Nupur Garg
+
+
+# Run Code
+
+     cd src
+     python decomposer.py <PYTHON-2-FILE>
+     python3 decomposer.py <PYTHON-3-FILE>
---
+++ src/generatecfg.py
@@ -0,0 +1,36 @@
+# File name: generatecfg.py
+# Author: Nupur Garg
+# Date created: 12/25/2016
+# Date last modified: 12/25/2016
+# Python Version: 3.5
+
+
+from __future__ import print_function
+import ast
+
+
+# Visitor to generate CFG.
+class CFGGenerator(ast.NodeVisitor):
+   """
+   Generates a CFG from an AST.
+
+   debug: bool
+      Whether to print debug messages.
+   """
+
+   temp = None
+
+   def __init__(self, debug):
+      self.debug = debug
+
+   # Generates CFG.
+   def generate(self, node):
+      # TODO: Initialize variables.
+      self.visit(node)
+      # TODO: Return variables.
+      print("returned: ", self.temp)
+      return "TEMP"
+
+   def visit_Str(self, node):
+      self.temp = "testing"
+      print('line %d: (str) "%s"' %(node.lineno, node.s))
