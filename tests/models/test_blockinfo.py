991116e52cdcfbbdf0e36152112bb9e4f3f2717f
ngarg <ngarg@calpoly.edu>
2017-05-02 00:18:08 -0700

    Added tests to blockinfo.

--- tests/models/test_blockinfo.py
+++ tests/models/test_blockinfo.py
@@ -5,8 +5,11 @@
 
 
 import unittest
+import ast
 
 from src.globals import *
+from src.generatecfg import CFGGenerator
+from src.models.block import Block
 from src.models.blockinfo import *
 
 
@@ -96,38 +99,106 @@ class TestLiveVariables(unittest.TestCase):
 # Test FunctionBlockInformation class.
 class TestFunctionBlockInformation(unittest.TestCase):
 
+    def setUp(self):
+        Block._label_counter.reset()
+
+    def _get_func_block(self):
+        source = ('def funcA():\n'                      # line 1
+                  '     a = 5\n'                        # line 2
+                  '     hpixels = 5\n'                  # line 3
+                  '     wpixels = 10\n'                 # line 4
+                  '     for y in range(5):\n'           # line 5
+                  '         for x in range(2):\n'       # line 6
+                  '             hpixels += 1\n'         # line 7
+                  '             new_var = 0\n'          # line 8
+                  '         wpixels += 1\n'             # line 9
+                  '     print(hpixels)\n')              # line 10
+        node = ast.parse(source)
+        generator = CFGGenerator(False)
+        cfg = generator.generate(node, source)
+        func = cfg.get_func('funcA')
+        return func
+
     def test_equality(self):
         info1 = FunctionBlockInformation()
         info2 = FunctionBlockInformation()
-        self.assertFalse(info1 == None)
-        self.assertFalse(info1 == 1)
+        func = self._get_func_block()
+        self.assertNotEqual(info1, None)
+        self.assertNotEqual(info1, 1)
 
-        self.skipTest('TODO: Implement (Important)')
-        # TODO: Checks block info class equality.
-        # TODO: Checks blocks equality.
-        # TODO: Checks block info keys equality.
-        # TODO: Checks block info values equality.
-        # TODO: Checks instruction info values equality.
+        # Test equality of blockinfo type blocks.
+        info1.init(func, LiveVariables)
+        info2.init(func, ReachingDefinitions)
+        self.assertNotEqual(info1, info2)
+        info2.init(func, LiveVariables)
+        self.assertEqual(info1, info2)
 
     def test_init(self):
-        self.skipTest('TODO: Implement (Important)')
-        # TODO: Checks block_info has correct keys
-        # TODO: Checks instructions, instruction_info has correct keys
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        # Checks general information equal.
+        self.assertEqual(info._block_info_class, LiveVariables)
+
+        # Checks keys for block info.
+        keys = set(['funcA', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7'])
+        self.assertEqual(set(info._block_info.keys()), keys)
+
+        # Checks keys for instructions.
+        keys = set(range(1, 11))
+        self.assertEqual(set(info._instructions.keys()), keys)
+        self.assertEqual(set(info._instruction_info.keys()), keys)
 
     def test_blocks(self):
-        self.skipTest('TODO: Implement (Important)')
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        blocks = info.blocks()
+        self.assertEqual(blocks[0][0].label, 'funcA')
+        self.assertEqual(blocks[1][0].label, 'L2')
+        self.assertEqual(blocks[2][0].label, 'L3')
+        self.assertEqual(blocks[3][0].label, 'L5')
+        self.assertEqual(blocks[4][0].label, 'L6')
+        self.assertEqual(blocks[5][0].label, 'L7')
+        self.assertEqual(blocks[6][0].label, 'L4')
+        self.assertEqual(blocks[7][0].label, 'L1')
 
     def test_instructions(self):
-        self.skipTest('TODO: Implement (Important)')
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        instructions = info.instructions()
+        linenos = [instr.lineno for instr, info in instructions]
+        self.assertEqual(linenos, list(range(1, 11)))
 
     def test_get_block_info(self):
-        self.skipTest('TODO: Implement (Important)')
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        block_info = info.get_block_info(func)
+        self.assertEqual(type(block_info), LiveVariables)
 
     def test_get_instruction(self):
-        self.skipTest('TODO: Implement (Important)')
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        instr = info.get_instruction(1)
+        self.assertEqual(instr.lineno, 1)
+        self.assertFalse(info.get_instruction(13))
 
     def test_get_instruction_info(self):
-        self.skipTest('TODO: Implement (Important)')
+        info = FunctionBlockInformation()
+        func = self._get_func_block()
+        info.init(func, LiveVariables)
+
+        instr_info = info.get_instruction_info(1)
+        self.assertEqual(type(instr_info), LiveVariables)
+        self.assertFalse(info.get_instruction_info(13))
 
 
 if __name__ == '__main__':
